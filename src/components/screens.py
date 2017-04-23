2b412c571932f56480fbe19c4cf459ad9d8beecd
Taylor Murias <taylor@murias.ca>
2017-04-23 14:34:01 -0700

    Added red square spawning

--- src/components/screens.py
+++ src/components/screens.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from .. data import constants
 from buttons import Button
 from squares import GoodSquare, BadSquare
@@ -47,21 +48,70 @@ class GameScreen(Screen):
     def __init__(self, on_death):
         self.on_death = on_death
 
-        # Sprite containers to make collision detection and rendering easier
         self.bad_squares = pygame.sprite.Group()
-        self.all_squares = pygame.sprite.Group()
 
         # Spawn the good square
         self.good_square = GoodSquare(constants.DISPLAY_WIDTH / 2,
                                       constants.DISPLAY_HEIGHT / 2,
                                       constants.GOOD_SQUARE_SPEED, 0)
-        self.all_squares.add(self.good_square)
+
+        for i in range(0, 10):
+            bad_square = BadSquare()
+            self.bad_squares.add(bad_square)
 
         self.score = 0
 
+        # Keeps track of the last time a bad square was spawned
+        self.time_since_spawn = 0
+
     def update(self, events, elapsed_time):
-        for square in self.all_squares:
+        self.time_since_spawn += elapsed_time
+
+        if self.time_since_spawn >= 1000:
+            self.spawn_bad_square()
+            self.time_since_spawn = 0
+
+        self.good_square.update(events, elapsed_time)
+        for square in self.bad_squares:
             square.update(events, elapsed_time)
 
     def render(self, game_display):
-        self.all_squares.draw(game_display)
+        self.good_square.render(game_display)
+        for bad_square in self.bad_squares:
+            if bad_square.active:
+                bad_square.render(game_display)
+
+    def spawn_bad_square(self):
+        for bad_square in self.bad_squares:
+            if not bad_square.active:
+                bad_square.active = True
+                side = random.randint(0, 3)
+                if side == 0:
+                    # Spawn on left
+                    y_slot = random.randint(0, 9)
+                    bad_square.position_in_x_slot(-1)
+                    bad_square.position_in_y_slot(y_slot)
+                    bad_square.x_speed = constants.BAD_SQUARE_SPEED
+                    bad_square.y_speed = 0
+                elif side == 1:
+                    # Spawn on right
+                    y_slot = random.randint(0, 9)
+                    bad_square.position_in_x_slot(15)
+                    bad_square.position_in_y_slot(y_slot)
+                    bad_square.x_speed = -constants.BAD_SQUARE_SPEED
+                    bad_square.y_speed = 0
+                elif side == 2:
+                    # Spawn on top
+                    x_slot = random.randint(0, 14)
+                    bad_square.position_in_x_slot(x_slot)
+                    bad_square.position_in_y_slot(-1)
+                    bad_square.x_speed = 0
+                    bad_square.y_speed = constants.BAD_SQUARE_SPEED
+                elif side == 3:
+                    # Spawn on bottom
+                    x_slot = random.randint(0, 14)
+                    bad_square.position_in_x_slot(x_slot)
+                    bad_square.position_in_y_slot(10)
+                    bad_square.x_speed = 0
+                    bad_square.y_speed = -constants.BAD_SQUARE_SPEED
+                break
--- src/components/squares.py
+++ src/components/squares.py
@@ -44,15 +44,41 @@ class GoodSquare(pygame.sprite.Sprite):
         elif self.rect.y > constants.DISPLAY_HEIGHT - height:
             self.rect.y = constants.DISPLAY_HEIGHT - height
 
+    def render(self, game_display):
+        game_display.blit(self.image, self.rect)
+
 
 class BadSquare(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0, x_speed=0, y_speed=0, active=False):
         super(BadSquare, self).__init__()
-        self.image = pygame.image.load('res/images/bad_square.png').convert()
+        self.image = pygame.image.load('res/images/bad_square.png').convert_alpha()
         self.image.set_colorkey(constants.WHITE)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.active = active
 
     def update(self, events, elapsed_time):
-        pass
+        self.rect.x += self.x_speed
+        self.rect.y += self.y_speed
+
+        # Square is no longer active if it is off the screen
+        width = self.rect.size[0]
+        height = self.rect.size[1]
+        off_left = (self.rect.x < -width) and (self.x_speed < 0)
+        off_right = (self.rect.x > constants.DISPLAY_WIDTH) and (self.x_speed > 0)
+        off_top = (self.rect.y < -height) and (self.y_speed < 0)
+        off_bottom = (self.rect.y > constants.DISPLAY_HEIGHT) and (self.y_speed > 0)
+        if off_left or off_right or off_top or off_bottom:
+            self.active = False
+
+    def render(self, game_display):
+        game_display.blit(self.image, self.rect)
+
+    def position_in_x_slot(self, x_slot):
+        self.rect.x = x_slot * self.rect.size[0]
+
+    def position_in_y_slot(self, y_slot):
+        self.rect.y = y_slot * self.rect.size[1]
